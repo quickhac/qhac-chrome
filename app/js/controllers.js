cbddaaa696e530e4463a72e586845287407816c7
Yash Aggarwal <yash0609@gmail.com>
2013-12-26 23:45:17 -0600

    get some functionality, only RRISD because weirdness

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,9 +15,11 @@ angular.module('myApp.controllers', []).
       GradeRetriever.login(district_information, username, password, id, function() {
         GradeRetriever.getAverages(district_information, function(data) {
           var gradeData = GradeParser.parseAverages(district_information, data);
-          GradeService.setGrades(gradeData, id);
-          console.log($scope);
-          $scope.message = "Logging in was a success.";
+          GradeService.setOriginalGrades(id, gradeData);
+          GradeService.setUserInformation(id, 'district', district_information); // zees is not good todo bug fixme, doesn't preserve the methods??!!
+          GradeService.setUserInformation(id, 'id', id);
+          GradeService.setUserInformation(id, 'username', username);
+          GradeService.setUserInformation(id, 'password', password);
           $location.path("/user/" + id + "/cycle/3");
           $scope.$apply();
         });
@@ -27,7 +29,24 @@ angular.module('myApp.controllers', []).
       });
     };
   }])
-  .controller('CycleController', ['$scope', '$routeParams', 'GradeService', function($scope, $routeParams, GradeService) {
+  .controller('CycleController', ['$scope', '$routeParams', 'GradeService', '$location', function($scope, $routeParams, GradeService, $location) {
     $scope.user_id = $routeParams.user_id;
     $scope.cycle_number = $routeParams.cycle_number;
+    $scope.overall_courses = GradeService.getOverallCycleInformation($scope.user_id, $scope.cycle_number);
+    $scope.next_cycle = parseInt($scope.cycle_number) + 1;
+    $scope.previous_cycle = parseInt($scope.cycle_number) - 1;
+    $scope.course_data = {};
+    $scope.goToNextCycle = function() {
+      $location.path("/user/" + $scope.user_id + "/cycle/" + $scope.next_cycle);
+    };
+    $scope.goToPreviousCycle = function() {
+      $location.path("/user/" + $scope.user_id + "/cycle/" + $scope.previous_cycle);
+    };
+
+    $scope.viewDetailed = function(course_id) {
+      GradeService.getInformationSpecificCycleCourse($scope.user_id, course_id, $scope.cycle_number, function(data) {
+        $scope.course_data = data; // incomplete fixme
+        $scope.$apply();
+      });
+    };
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -8,14 +8,76 @@
 angular.module('myApp.services', []).
   factory('GradeService', function() {
     var GradeService = {};
-    GradeService.getRawGrades = function() {
-      return store.get('qhac-grades');
+
+    GradeService.setUserInformation = function(id, key, value) {
+      var users = store.get('qhac-users') || {};
+      var info = GradeService.getUserInformation(id);
+      info[key] = value;
+      users[id] = info;
+      store.set('qhac-users', users);
     };
-    GradeService.setGrades = function(json, id) {
+
+    GradeService.getUserInformation = function(id) {
+      var users = store.get('qhac-users');
+      if(users === undefined) { store.set('qhac-users', {}); GradeService.getUserInformation(id); }
+      var user = users[id];
+      return user || {};
+    }
+    GradeService.getOriginalGrades = function(id) {
+      return store.get('qhac-grades-original-' + id);
+    };
+
+    GradeService.setOriginalGrades = function(id, json) {
+      console.log(json);
+      console.log(id);
+      store.set('qhac-grades-original-' + id, json);
       store.set('qhac-grades-' + id, json);
     };
+
     GradeService.getGrades = function(id) {
-      store.get('qhac-grades-' + id);
-    }
+      return store.get('qhac-grades-' + id);
+    };
+
+    GradeService.setGrades = function(id, json) {
+      store.set('qhac-grades-' + id, json);
+    };
+
+    GradeService.revertChanges = function(id) {
+      this.setGrades(id, this.getOriginalGrades());
+    };
+
+    GradeService.getCycle = function(course, cycleNumber) {
+      var semesterIndex = cycleNumber <= 3 ? 0 : 1;
+      var cycleIndex = (cycleNumber > 3 ? cycleNumber - 3 : cycleNumber) - 1;
+      return course.semesters[semesterIndex].cycles[cycleIndex];
+    };
+
+    GradeService.getOverallCycleInformation = function(id, cycleNumber) {
+      if(cycleNumber > 6 || cycleNumber < 1) { return false; }
+      var json = this.getGrades(id);
+      return _.map(json, function(course) {
+        var courseName = course.title;
+        var id = course.courseId;
+        var average = GradeService.getCycle(course, cycleNumber).average;
+        return { id: id, name: courseName, average: average};
+      });
+    };
+
+    GradeService.getInformationSpecificCycleCourse = function(id, courseId, cycleNumber, success, fail) {
+      //var district = GradeService.getUserInformation(id).district;
+      var district = Districts.roundrock; // debug todo wtf
+      var json = this.getGrades(id);
+      var course = _.find(json, function(course) {
+        return course.courseId === courseId;
+      });
+      var cycle = this.getCycle(course, cycleNumber);
+      if(cycle.urlHash === undefined) { return false; }
+      GradeRetriever.getClassGrades(district, cycle.urlHash, null, function (html) {
+        success(GradeParser.parseClassGrades(district, html, cycle.urlHash, null, null));
+      }, function () {
+        fail(); // todo redo
+      });
+    };
+        
     return GradeService;
   });
